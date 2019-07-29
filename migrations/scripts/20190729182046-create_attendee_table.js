5cf188ad641f299e0333ddb3fb837fd130e52e72
Brent Wilson <brent@prmconnect.com>
2019-07-29 15:32:00 -0700

    added attendee migration

--- migrations/scripts/20190729182046-create_attendee_table.js
+++ migrations/scripts/20190729182046-create_attendee_table.js
@@ -9,9 +9,56 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
+    return queryInterface.createTable('attendee', {
+      RegId: {
+        type: Sequelize.STRING,
+        primaryKey: true
+      },
+      ContactId:{
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      }, 
+      CampaignId: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      KioskConfigurationId: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      SiteId: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      ContactObj: Sequelize.JSONB,
+      CheckInDateTime: Sequelize.STRING,
+      LastCheckInDateTime: Sequelize.STRING,
+      PrintDateTime: Sequelize.STRING,
+      RegisterDateTime: Sequelize.STRING,
+      PrintCount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      CheckedIn: Sequelize.BOOLEAN,
+      UpsertStatus: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true 
+      },
+      Deleted: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      fromScan: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
   },
 
-  down: (queryInterface, Sequelize) => {
+  down: (queryInterface) => {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
@@ -19,5 +66,6 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
+    return queryInterface.dropTable('attendee');
   }
 };
